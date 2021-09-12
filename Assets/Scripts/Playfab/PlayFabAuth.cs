c1035973d6ecfda18216ff6d3c0a3cf34e58103b
William <william.bertrand94@gmail.com>
2021-09-11 19:54:54 -0600

    Set up PlayFab SDK and add some basic menu assets

---
+++ Assets/Scripts/Playfab/PlayFabAuth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayFabAuth : MonoBehaviour
+{
+
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private TMP_InputField passwordInput;
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TMP_Text infoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
